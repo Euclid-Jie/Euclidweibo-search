8a9e104c033fc871ed91c754830b92b4395ede23
欧玮杰 <ouweijie123@outlook.com>
2023-02-04 20:33:06 +0800

    docs(update cookie and readme):

--- WeiboClassRun.py
+++ WeiboClassRun.py
@@ -6,11 +6,15 @@ from WeiboClass import WeiboClass
 from WeiboClassMongo import WeiboClassMongo
 
 # 设置参数
-keyList = ['北师大', '北京师范大学', '北京师范大学统计学院', 'BNU', '北师', '北京师范大学珠海校区']
-# keyList = ['#北京师范大学120周年校庆#']
+# keyList = ['北师大', '北京师范大学', '北京师范大学统计学院', 'BNU', '北师', '北京师范大学珠海校区']
+keyList = ['九寨沟']
 # keyList = ['美团民宿']
-timeBegin = '2023-01-01-0'
-timeEnd = '2023-02-01-0'
-# 运行函数
+timeBegin = '2020-01-01-0'
+timeEnd = '2020-02-01-0'
+# 运行函数[两种写入方法二选一]
+
+# 1、使用数据库写入
 # WeiboClass(keyList, timeBegin, timeEnd, limit=3, contains=True).main_get()
+
+# 2、使用csv写入
 WeiboClassMongo(keyList, timeBegin, timeEnd, limit=3, contains=True).main_get()
