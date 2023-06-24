765f5ba4a2ea691463d82589b3ca6696c22e5b96
欧玮杰 <ouweijie123@outlook.com>
2023-06-24 15:58:09 +0800

    refactor(适用于北师大舆情分析版本): 配置cookie后，运行WeiboClassRun即可

--- WeiboClassRun.py
+++ WeiboClassRun.py
@@ -3,9 +3,8 @@
 # file: WeiboClassRun.py
 # author: Euclid_Jie
 import pymongo
-
-from WeiboClass import WeiboClass
-from WeiboClassMongo import WeiboClassMongo
+from datetime import datetime
+from WeiboClassV1 import WeiboClassV1
 import pandas as pd
 
 
@@ -42,21 +41,20 @@ def read_mongo(DBName, collectionName, query=None, no_id=True):
 
 # 设置参数
 keyList = ['北师大', '北京师范大学', '北京师范大学统计学院', 'BNU', '北师', '北京师范大学珠海校区']
-# keyList = ['马航飞机失联', '马航370', 'MH370']
-# keyList = ['杭州公园']
-# keyList = ['美团民宿']
-timeBegin = '2023-03-01-0'
-timeEnd = '2023-03-10-0'
+timeBegin = '2023-05-01-0'
+timeEnd = '2023-06-01-0'
 
 if __name__ == '__main__':
     # 运行函数[两种写入方法二选一]
     # 1、使用csv写入
-    # WeiboClass(keyList, timeBegin, timeEnd, limit=3, contains=True).main_get()
-    # 2、使用MongoDB数据库
-    WeiboClassMongo(keyList, timeBegin, timeEnd, limit=3, contains=True).main_get()
+    WeiboClassV1(limit=3, contains=True, Mongo=False).main_get(keyList, timeBegin, timeEnd)
+    # 2、使用用MongoDB数据库
+    # WeiboClassV1(limit=3, contains=True, Mongo=True).main_get(keyList, timeBegin, timeEnd)
 
     # ---------------读取MongoDB数据至CSV
     # for key in keyList:
     #     df = read_mongo('Weibo', key, query=None, no_id=True)
-    #     df = df[df['time'].str.contains('02月')].copy()
-    #     df.to_csv(r'outData\2023-02-{}.csv'.format(key), index=False, encoding='utf-8-sig')
+    #     df = df.loc[df['time'].apply(lambda x: isinstance(x, datetime))]
+    #     # df = df[df['time'].str.contains('02月')].copy()
+    #     df = df.loc[df['time'] >= pd.to_datetime("2023-05-01")]
+    #     df.to_csv(r'outData\2023-05-{}.csv'.format(key), index=False, encoding='utf-8-sig')
